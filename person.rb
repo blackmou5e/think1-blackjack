632c1ecb2da1042a094d261d0238718453107ccc
George Nikishin <mail2giz@gmail.com>
2022-07-17 23:00:39 +0300

    initial person class

---
+++ person.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+# Base class for participants
+class Person
+  attr_reader :money
+
+  def initialize(money)
+    @money = money
+    @hand = []
+    @points = 0
+  end
+
+  def check
+    true
+  end
+
+  def take_card(card)
+    @hand << card
+  end
+
+  def make_bet
+    @money -= 10
+
+    10
+  end
+
+  def take_prize(value)
+    @money += value
+  end
+
+  def free_hand
+    @hand = []
+  end
+
+  protected
+
+  def calculate_points
+    sum = 0
+    ace_count = 0
+    @hand.each do |card|
+      if card.name == 'A'
+        ace_count += 1
+        next
+      end
+      sum += card.value
+    end
+
+    while ace_count >= 1
+      sum += sum <= 10 ? 11 : 1
+      ace_count -= 1
+    end
+
+    @points = sum
+  end
+  # sum = 0
+  # @hand.each { |card| sum += card.value }
+  # @points = sum
+
+  def show_hand
+    @hand
+  end
+
+  def show_points
+    @points
+  end
+end
